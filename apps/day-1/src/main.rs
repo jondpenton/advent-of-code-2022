ed91e8ae9d9a6fa5d79b9c0a0440af4062c4c519
Jon Penton <hey@jondpenton.com>
2022-12-01 18:42:32 -0500

    feat: :sparkles: day 1 part 0

--- apps/day-1/src/main.rs
+++ apps/day-1/src/main.rs
@@ -1,3 +1,37 @@
+use std::fs;
+
 fn main() {
   println!("Hello, world!");
+
+  println!("{:#?}", get_max_elf_calories())
+}
+
+fn get_max_elf_calories() -> u64 {
+  let contents = fs::read_to_string("assets/part0.txt")
+    .expect("Should have been able to read the file");
+  let elves = contents.trim().split("\n\n").map(|elf| {
+    elf.split("\n").map(|calories| {
+      calories
+        .parse::<u64>()
+        .expect(format!("{:#?} should have been an int.", calories).as_str())
+    })
+  });
+  let max_elf_calories = elves.fold(0, |total, elf| {
+    match elf.fold(0, |calorie, total| calorie + total) {
+      calories if calories > total => calories,
+      _ => total,
+    }
+  });
+
+  return max_elf_calories;
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn part0() {
+    assert_eq!(get_max_elf_calories(), 24000)
+  }
 }
